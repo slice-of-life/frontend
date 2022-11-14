b41e5e4644e4f91e588929665e1c16177a811884
Matthew Isayan <misayan@gmail.com>
2022-11-14 02:42:31 -0800

    Fixed up feed.

--- slice-of-life/src/app/pages/feed/feed.component.ts
+++ slice-of-life/src/app/pages/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
 
 @Component({
   selector: 'feed',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
-
+  feedPosts : Post[];
   constructor() { }
 
   ngOnInit(): void {
