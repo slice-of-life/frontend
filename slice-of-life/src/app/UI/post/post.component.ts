0180b42052bb17f9ef4bf1991c072e3011bb5384
Matthew Isayan <misayan@gmail.com>
2022-11-19 21:25:47 -0800

    Updated Feed and Post components for dynamic content

--- slice-of-life/src/app/UI/post/post.component.ts
+++ slice-of-life/src/app/UI/post/post.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post.model';
-
+import moment from 'moment';
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
   @Input() post: Post;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  calcTime() {
+    const postDate = new Date(this.post.created_at);
+    return moment(postDate).fromNow();
   }
-
 }
