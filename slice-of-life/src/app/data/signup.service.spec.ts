cff9a8d58bd1212a0324bc98f140a74ed4452e28
Matthew Isayan <misayan@gmail.com>
2022-12-11 00:38:05 -0800

    Added test case for signup service using a mock

---
+++ slice-of-life/src/app/data/signup.service.spec.ts
@@ -0,0 +1,80 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { SignupService } from './signup.service';
+import { User } from '../models/user.model';
+import { fakeAsync, tick } from '@angular/core/testing';
+const baseURL =
+  'https://sliceoflife-webservice-d7uk9.ondigitalocean.app/api/v1/';
+
+describe('SignupService', () => {
+  let signupService: SignupService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SignupService],
+    });
+
+    signupService = TestBed.inject(SignupService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should create a new user account', fakeAsync(() => {
+    const user: User = {
+      handle: 'handle',
+      email: 'email',
+      password: 'password',
+      first_name: 'first_name',
+      last_name: 'last_name',
+    };
+
+    let response;
+    signupService.createUser(user).subscribe((res) => (response = res));
+
+    const req = httpTestingController.expectOne(baseURL + 'users/account/new');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(user);
+    req.flush({ status: 'CREATED' });
+    tick();
+    expect(response).toEqual({ status: 'CREATED' });
+  }));
+
+  it('should return an error if the handle is unavailable', fakeAsync(() => {
+    const user: User = {
+      handle: 'handle',
+      email: 'email',
+      password: 'password',
+      first_name: 'first_name',
+      last_name: 'last_name',
+    };
+
+    let testError;
+    let testResponse;
+    signupService.createUser(user).subscribe({
+      error: (err) => {
+        testError = err;
+      },
+    });
+
+    const req = httpTestingController.expectOne(baseURL + 'users/account/new');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(user);
+    req.flush("", {
+      status: 403,
+      statusText: 'handle not available'
+    });
+    tick();
+    expect({status: testError.status, statusText: testError.statusText}).toEqual({
+      status: 403,
+      statusText: 'handle not available'
+    });
+  }));
+});
