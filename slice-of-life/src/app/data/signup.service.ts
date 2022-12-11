a98108d38cbec99b1c6054f662a3d972748db173
Matthew Isayan <misayan@gmail.com>
2022-12-11 00:39:29 -0800

    Changed createUser to accept a User object

--- slice-of-life/src/app/data/signup.service.ts
+++ slice-of-life/src/app/data/signup.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 const baseURL =
   'https://sliceoflife-webservice-d7uk9.ondigitalocean.app/api/v1/';
 
@@ -8,13 +9,13 @@ const baseURL =
 })
 export class SignupService {
   constructor(private http: HttpClient) {}
-  createUser(handle: string, email: string, password: string, first_name: string, last_name: string) {
+  createUser(user: User) {
     return this.http.post(baseURL + 'users/account/new', {
-      handle,
-      email,
-      password,
-      first_name,
-      last_name
+      'handle' : user.handle,
+      'email' : user.email,
+      'password' : user.password,
+      'first_name' : user.first_name,
+      'last_name' : user.last_name
     });
 }
 }
