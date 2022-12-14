c06278d14b712f547df8dab7976552821424c0e0
Matthew Isayan <misayan@gmail.com>
2022-12-14 03:09:25 -0800

    handling http response and error

--- slice-of-life/src/app/pages/signup/signup.component.ts
+++ slice-of-life/src/app/pages/signup/signup.component.ts
@@ -1,48 +1,59 @@
+import { HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SignupService } from 'src/app/data/signup.service';
+import { AlertType } from 'src/app/UI/alert/alert.enum';
 
 @Component({
   selector: 'signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  httpCode: HttpStatusCode;
+  responseType : AlertType;
+  responseMessage : string;
 
-  constructor(private signupService: SignupService) { }
+  constructor(private signupService: SignupService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   signupForm = new FormGroup({
-    email : new FormControl('', [Validators.required]),
-    firstName : new FormControl('', [Validators.required]),
-    lastName : new FormControl('', [Validators.required]),
-    password : new FormControl('', [Validators.required]),
-    confirmPassword : new FormControl('',[Validators.required]),
-    handle : new FormControl('', [Validators.required])
-  
-  })
-  
+    email: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    handle: new FormControl('', [Validators.required]),
+  });
+
   getErrorMessage() {
-
     if (this.signupForm.get('email').hasError('required')) {
-      
     }
-    
 
-    return 'You must enter a value';;
+    return 'You must enter a value';
   }
   signupUser() {
-    console.log("singing up handle" + this.signupForm.get('handle').value);
-  //   this.signupService.createUser('handle', 'email', 'password', 'first_name', 'last_name')
-  //   .subscribe({
-  //     next: response => {
-  //       // Handle the "CREATED" response
-  //     },
-  //     error: error => {
-  //       // Handle the error, such as a "403" response for an unavailable handle
-  //     }
-  // });
-  
+    console.log('singing up handle: ' + this.signupForm.get('handle').value);
+    this.signupService
+      .createUser({
+        handle: this.signupForm.get('handle').value,
+        email: this.signupForm.get('email').value,
+        password: this.signupForm.get('password').value,
+        first_name: this.signupForm.get('firstName').value,
+        last_name: this.signupForm.get('lastName').value,
+      })
+      .subscribe({
+        next: (response) => {
+          console.log('Response: ' + response);
+          this.responseMessage = 'Success acount created';
+          this.responseType = AlertType.Success;
+
+        },
+        error: (error) => {
+          console.log('Error: ' + error);
+          this.responseMessage = 'Handle is unavailable, try another handle.';
+          this.responseType = AlertType.Failure;
+        },
+      });
   }
 }
