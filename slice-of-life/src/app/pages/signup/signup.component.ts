3987bd7ac2b61c9ffd39e5d12ce0c020cec5f0c9
Matthew Isayan <misayan@gmail.com>
2022-12-20 08:14:14 -0800

    Added proper form validators and proper behavior

--- slice-of-life/src/app/pages/signup/signup.component.ts
+++ slice-of-life/src/app/pages/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SignupService } from 'src/app/data/signup.service';
 import { AlertType } from 'src/app/UI/alert/alert.enum';
 
@@ -11,27 +12,77 @@ import { AlertType } from 'src/app/UI/alert/alert.enum';
 })
 export class SignupComponent implements OnInit {
   httpCode: HttpStatusCode;
-  responseType : AlertType;
-  responseMessage : string;
-  loading : boolean = false;
-  constructor(private signupService: SignupService) {}
+  responseType: AlertType;
+  responseMessage: string;
+  loading: boolean = false;
+  unavailableHandles : String[] = [];
 
-  ngOnInit(): void {}
-  signupForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
-    handle: new FormControl('', [Validators.required]),
+  signupForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    handle: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+    
   });
 
-  getErrorMessage() {
-    if (this.signupForm.get('email').hasError('required')) {
-    }
+  constructor(private signupService: SignupService, private fb: FormBuilder, private router :Router) {}
 
-    return 'You must enter a value';
+  ngOnInit(): void {}
+
+  getErrorMessage(controlName: string) {
+    const control = this.signupForm.get(controlName);
+    if (control.hasError('email')) {
+      return 'Invalid email address.';
+    } 
+    else if (control.hasError('maxlength')) {
+      return 'Maximum of 40 characters allowed.';
+    } 
+    else if (control.hasError('confirm')) {
+      return 'Password does not match above.';
+    }
+    else if (control.hasError('handle')){
+      return 'Handle not available.';
+    }
+    return 'You must enter a value.';
   }
+  
+  hasErrors() {
+    for(let x in this.signupForm.controls){
+      if(this.signupForm.get(x).errors && this.signupForm.get(x).touched) {
+        return true;
+    }
+  }
+    return false;
+  }
+  confirmPassword() {
+    if(this.signupForm.get('password').value !== this.signupForm.get('confirmPassword').value) {
+      this.signupForm.get('confirmPassword').setErrors({confirm : true})
+    }
+    else {
+      this.signupForm.get('confirmPassword').setErrors(null)
+    }
+  }
+
+  submit() {
+    this.signupForm.markAllAsTouched();
+    if(this.signupForm.valid) {
+      this.signupUser();
+    }
+  }
+
+  handleError() {
+    this.unavailableHandles.push(this.signupForm.get('handle').value);
+    this.signupForm.get('handle').setErrors({ handle: true });
+  }
+
+  onChangeHandle() {
+    if(this.unavailableHandles.includes(this.signupForm.get('handle').value)){
+      this.signupForm.get('handle').setErrors({ handle: true });
+    }
+  }
+
   signupUser() {
     this.loading = true;
     this.signupService
@@ -44,15 +95,19 @@ export class SignupComponent implements OnInit {
       })
       .subscribe({
         next: (response) => {
-          this.responseMessage = 'Success acount created';
+          this.responseMessage = 'Success acount created, automatically redirecting you to the login page.';
           this.responseType = AlertType.Success;
-
+          setTimeout(() => this.router.navigateByUrl("/login"), 2500);
         },
-        error: (error : HttpErrorResponse) => {
-          this.responseMessage = error.status === 401 ? 'Handle not available, please try another handle.' : `Received error: ${error.statusText}`;
+        error: (error: HttpErrorResponse) => {
+          this.responseMessage =
+            error.status === 401
+              ? 'Handle not available, please try another handle.'
+              : `Received error: ${error.statusText}`;
+          if (error.status === 401) this.handleError()
           this.responseType = AlertType.Warning;
-          this.loading = false;
-        }
-      }).add(() => this.loading = false)
+        },
+      })
+      .add(() => (this.loading = false));
   }
 }
