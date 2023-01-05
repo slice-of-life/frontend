5727ce8796cfa16bff647763ad2fdaface28b428
Matthew Isayan <misayan@gmail.com>
2023-01-04 22:04:01 -0800

    Removed log statements before merging to main branch

--- slice-of-life/src/app/pages/login/login.component.ts
+++ slice-of-life/src/app/pages/login/login.component.ts
@@ -37,12 +37,4 @@ export class LoginComponent implements OnInit {
       }
     );
   }
-
-  logUserInfo() {
-    this.loginService.getUserInfo().subscribe(
-      (response) => {
-        console.log(response);
-      }
-    );
-  }
 }
